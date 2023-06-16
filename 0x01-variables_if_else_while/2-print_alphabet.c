47545a6edd8b0fe04e76ae8cf081c3f737648059
Nada Yassin <yassinada19@gmail.com>
2023-06-16 16:28:49 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point and prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+
+	{
+		putchar(alphabet[i]);
+		putchar('\n');
+	}
+
+	return (0);
+}
