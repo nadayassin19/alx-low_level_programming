2d6920176ceaa414ef3ec1fda9300f3640b9d8b9
Nada Yassin <yassinada19@gmail.com>
2023-06-16 17:08:24 -0700

    task5 edit1

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,13 +9,12 @@
 
 int main(void)
 {
-	int s[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i = 0;
 
 	for (; i < 10; i++)
 	{
-		printf(s[i]);
+		printf(i);
 	}
-	printf('\n');
+	putchar('\n');
 	return (0);
 }
