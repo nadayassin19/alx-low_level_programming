82c56564b29f681fd3e37f01281290d8f276f030
Nada Yassin <yassinada19@gmail.com>
2023-06-16 17:20:13 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point and prints all single digit numbers
+ * of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar ('\n');
+	return (0);
+}
+
