b181bc616d081b2ceb13cb31b01dc463945132f2
Nada Yassin <yassinada19@gmail.com>
2023-06-20 13:09:49 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point and implements print_alphabet func
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ *print_alphabet - prints the alphabet in lowercase.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
