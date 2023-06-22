7e0859930876d5918c1c9df88204a40407c5c7d1
Nada Yassin <yassinada19@gmail.com>
2023-06-22 10:26:05 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _isupper - checks if parameter is an uppercase character.
- * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
+ * _isupper - checks for uppercase character.
+ *
+ * Return: 1 if the letter is uppercase
+ *	0 if the letter is not uppercase.
+ *
+ * @c: is the letter to be checked in ASCII code.
  */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
