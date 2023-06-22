8807cc30561f7586115770812bdfc986556b897a
Nada Yassin <yassinada19@gmail.com>
2023-06-22 05:13:58 -0700

    task0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point & tests the function that prints
+ * whether the number is -ve or 0 or +ve.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+0.  test that the function positive_or_negative() gives the correct output when given a case of 0.
+
+1. 
