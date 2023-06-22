50c2001176c0ade716f16c908c04199579b7749b
Nada Yassin <yassinada19@gmail.com>
2023-06-22 13:39:26 -0700

    task9 edit3

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,12 +5,13 @@
  *
  * Return: Always 0 (Success).
  */
-
 int main(void)
 {
 	int n;
 
-	for (n = 1; n <= 100; n++)
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
