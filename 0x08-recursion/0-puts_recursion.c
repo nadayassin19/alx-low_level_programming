30f187a7aede4ca60aa52daf98d48b6bb279f707
Nada Yassin <yassinada19@gmail.com>
2023-07-05 10:36:11 -0700

    task0 edit1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,17 +11,10 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (i >= 0)
+	if (*s != '\0')
 	{
-		if (s[i] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(s[i]);
-		i++;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
