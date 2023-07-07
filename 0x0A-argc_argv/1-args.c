4c98f84537c6c77cda28d1f57aa6dd1bbb410089
Nada Yassin <yassinada19@gmail.com>
2023-07-07 13:14:23 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point & prints the number
+ * of arguments passed into it.
+ * 
+ * @argc: number of command line arguments.
+ * 
+ * @argv: array that contains the program command line arguments.
+ * 
+ * Return: Always 0 (Success).
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
