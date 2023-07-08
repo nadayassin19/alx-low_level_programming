ad88a0d0a63f0209d4be149549d67c98dcba9a1e
Nada Yassin <yassinada19@gmail.com>
2023-07-08 16:05:27 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,29 +2,45 @@
 
 /**
  * rev_string - reverses a string.
+ *
+ * Return: nothing.
+ *
  * @s: input string.
- * Return: no return.
  */
+
 void rev_string(char *s)
 {
-	int count = 0, i, j;
-	char *str, temp;
+	int length, i, j;
 
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
-	str = s;
+	length = _strlen(s);
+	i = j = 0;
+	char *newString, temp;
 
-	for (i = 0; i < (count - 1); i++)
+	newString = s;
+
+	for (; i < (length - 1); i++)
 	{
-		for (j = i + 1; j > 0; j--)
-		{
-			temp = *(str + j);
-			*(str + j) = *(str + (j - 1));
-			*(str + (j - 1)) = temp;
-		}
+		temp = s[i];
+		newString[length - i] = temp;
 	}
+}
+
+/**
+ * _strlen - a func gives the length of str.
+ *
+ * Return: the length of a str in int.
+ *
+ * @s: is the variable we want to know its length.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	for (; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
 }
