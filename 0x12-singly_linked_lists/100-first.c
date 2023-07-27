65d457154c575844db2df5e44457d6989667cf8e
Nada Yassin <yassinada19@gmail.com>
2023-07-27 06:44:33 -0700

    tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * bmain - function executed before main func,
+ * that prints a sentence.
+ *
+ * Return: nothing.
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
