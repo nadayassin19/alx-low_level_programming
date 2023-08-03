2dd5a9aaf92f0669dc5e2c813b7884b9ecd8e2ed
Nada Yassin <yassinada19@gmail.com>
2023-08-03 15:25:31 -0700

    task2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,20 @@
  *Return: the value of the bit at index index or -1 if an error occured.
  */
 
-get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int i;
 
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+
+	return (-1);
 }
