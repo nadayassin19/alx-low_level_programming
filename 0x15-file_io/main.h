6cde9e6e19a2b0f860d71128561fd78ff678d21f
Nada Yassin <yassinada19@gmail.com>
2023-08-07 14:58:11 -0700

    edit headerFile 1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,13 @@
-#ifndef
-#define
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define READ_BUF_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
